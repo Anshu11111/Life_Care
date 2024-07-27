6cef85c0c02819218d342dac9bd6257e72d129b3
anshu <anshuanand0901@gmail.com>
2024-07-27 18:42:42 +0530

    fronted done

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from 'react';
 
-function Contact() {
+const Contact = () => {
   return (
-    <div>
-      contact
-    </div>
-  )
-}
+    <section>
+      <div className="px-4 mx-auto max-w-screen-md">
+        <h2 className="heading text-center">Contact Us</h2>
+        <p className="mb-8 lg:mb-16 font-light text-center text_para">
+          Got a technical issue? Want to send feedback about a beta feature? Let us know.
+        </p>
+        <form action="#" className="space-y-8">
+          <div>
+            <label htmlFor="email" className="form_label">
+              Your Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="example@gmail.com"
+              className="form_input mt-1"
+              required
+            />
+          </div>
+          <div className='sm:col-span-2'>
+            <label htmlFor="subject" className="form_label">
+              Your Message
+            </label>
+            <textarea
+              id="message"
+              placeholder="Your message here..."
+              className="form_input mt-1"
+              rows="4"
+              required
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="px-6 py-3 bg-primaryColor text-white font-semibold rounded-md"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { services } from "../assets/data/services";
+import ServiceCard from "../components/Services/ServiceCard";
 
-function Services() {
+ const Services = () => {
   return (
-    <div>
-      services
+    <section>
+      <div className="container">
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gp-5 lg:gap-[30px]'>
+      {services.map((item, index)=>(
+        <ServiceCard item={item} index={index} key={index}/>
+        ))}
     </div>
-  )
-}
-
-export default Services
+      </div>
+    </section>
+  );
+};
+export default Services;
