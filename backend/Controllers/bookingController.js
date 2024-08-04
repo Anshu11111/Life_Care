4d982a2307c347b241b7d0914e911ccf8e579cef
Anshu Anand <anshuanand0901@gmail.com>
2024-08-05 01:11:32 +0530

    Update bookingController.js

--- backend/Controllers/bookingController.js
+++ backend/Controllers/bookingController.js
@@ -18,7 +18,7 @@ export const getCheckoutSession = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       mode: 'payment',
-      success_url: 'http://localhost:5173/checkout-success',
+      success_url: 'https://lifecare-doctor-appointment.netlify.app/checkout-success',
       cancel_url: `${req.protocol}://${req.get('host')}/doctors/${doctor.id}`,
       customer_email: user.email,
       client_reference_id: req.params.doctorId,
